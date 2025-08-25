ac9a4b157fd5503d0df91aa953c8b74e4b2ca272
swarnabha <swarnabhadutta909@gmail.com>
2025-08-25 16:40:29 +0530

    add Frontend and prismjs for syntax highlighting

---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+import { useState,useEffect} from 'react';
+
+import "prismjs/themes/prism-tomorrow.css";
+import prism from "prismjs"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    prism.highlightAll();
+  });
+
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <pre>
+              <code className='language-javascript'>
+{`const sum = () => {
+      return 1 + 1;
+}
+`}
+              </code>
+            </pre>
+          </div>
+          <div className="review">Review</div>
+        </div>
+        <div className="right"></div>
+      </main>
+    </>
+  )
+}
+
+
+
+export default App
