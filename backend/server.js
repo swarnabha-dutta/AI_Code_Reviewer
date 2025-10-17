8b8536d6e3fc205f97a3c220352c95520558326b
swarnabha <swarnabhadutta909@gmail.com>
2025-10-17 21:49:25 +0530

    Feat: Implement Multiple files uploading and MongoDB atlas connection properly

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,12 @@ const app = require("./src/app.js");
 const port = process.env.PORT || 3000; 
 
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/fileUploadCodeReview";
+
+
+if (!MONGO_URI) {
+    console.error("FATAL ERROR: MONGO_URI is not defined. Cannot connect to database.");
+    process.exit(1);
+}
 mongoose.connect(MONGO_URI)
     .then(() => {
         console.log(`MongoDB Connected Successfully! 💾`);
